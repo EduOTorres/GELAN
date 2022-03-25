eb11969c4aab698e332506221ba903fb3f278fed
Eduardo Torres <eduardootorrespro@hotmail.com>
2022-03-25 15:32:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var hamburguer = document.querySelector (".hamburguer")
+
+hamburguer.addEventListener("click", function name(params) {
+    document.querySelector (".container").classList.toggle("show-menu")   
+});
